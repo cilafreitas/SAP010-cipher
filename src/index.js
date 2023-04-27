c2178f4b4b7c0fdfea766bcbc5d5ae5113253028
cilafreitas <cila123xavier@gmail.com>
2023-04-27 12:05:29 -0300

    aprendendo js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 import cipher from './cipher.js';
+//manipulação de DOM
+
+// clicar botão
+// pegar o valor do input
+// fazer a validação e/ou criptografia - validort.js/cipher
+// retorno o valor para o usuario
+
+//seletores de DOM
 
 console.log(cipher);
