ee5c4824f111f165db1637f30ef7933b7a87675a
cilafreitas <cila123xavier@gmail.com>
2023-05-23 09:43:06 -0300

    Updates

--- cipher.js
+++ cipher.js
@@ -1,5 +1,57 @@
-const cipher = {
-  // ...
-};
+//encode and decode ASCII function
 
+
+const cipher = {
+  encode:function(desloc, texto){
+    if(typeof desloc !== 'number' || typeof texto !== 'string') //condição para ver se meu texto é um número e se meu texto é uma string
+    {throw new TypeError('os argumentos devem ser number e string')}
+    else{
+      let textoCodificado ="";  //variável que irá receber o texto codificado
+      for(let i = 0; i< texto.length; i++){ // Estrutura de repetição Laço For
+        let caracter = texto[i];
+        if(caracter.match(/[a-z]/i)){
+          const codigoAsc = texto.charCodeAt(i);
+          if(codigoAsc >=65 && codigoAsc <=90){
+            caracter = String.fromCharCode(((codigoAsc - 65 + desloc)% 26)+65); //Esta é a equação para codficar e decodificar//
+
+          }
+        }
+        textoCodificado += caracter;
+      }
+      return textoCodificado
+    }
+  },
+  
+  decode:function(desloc, texto){
+    if(typeof desloc !== 'number' || typeof texto !== 'string') //condição para ver se meu texto é um número e se meu texto é uma string
+    {throw new TypeError('os argumentos devem ser number e string')}
+    else{
+      let textoCodificado ="";  //variável que irá receber o texto codificado
+      for(let i = 0; i< texto.length; i++){ // Estrutura de repetição Laço For
+        let caracter = texto[i];
+        if(caracter.match(/[A-Z]/i)){
+          const codigoAsc = texto.charCodeAt(i);
+          if(codigoAsc >=65 && codigoAsc <=90){
+            let result= codigoAsc- 65 - desloc;
+            while(result < 0) {
+              result += 26;
+            }
+            caracter = String.fromCharCode((result % 26) + 65);
+          } else if (codigoAsc >= 97 && codigoAsc <= 122) {
+            let result = codigoAsc - 97 - desloc;
+            while (result < 0) {
+              result += 26;
+            }
+            caracter = String.fromCharCode((result % 26) + 97);
+          }
+        
+        }
+        textoCodificado += caracter;
+      
+      }
+      return textoCodificado
+    }
+  }
+  
+}
 export default cipher;
--- index.js
+++ index.js
@@ -1,3 +1,46 @@
 import cipher from './cipher.js';
+//console.log(cipher);//
 
-console.log(cipher);
+
+document.getElementById("codificar").addEventListener("click", codificar); //ouvinte de eventos + chamar a função//
+
+function codificar() {
+  const texto = document.getElementById("digite").value.toUpperCase(); // obtem o valor do campo de entrada e converte em letras maiúsculas//
+  const desloc = parseInt(document.getElementById("desloc").value); // função parseInt que converte o primeiro argumento para string, analisa e retorna um nº inteiro ou NaN/
+  const result = cipher.encode(desloc, texto);
+  document.getElementById("msgDecod").innerHTML="Mensagem codificada";
+  document.getElementById("result").value=result;
+}
+
+document.getElementById("decodificar").addEventListener("click", decodificar);
+
+function decodificar() {
+  const texto = document.getElementById("result").value; 
+  const desloc = parseInt(document.getElementById("desloc").value);
+  const result = cipher.decode(desloc, texto);
+  document.getElementById("msgDecod").innerHTML="Mensagem decodificada";
+  document.getElementById("result").value=result;
+}
+
+
+
+//manipulação de DOM
+
+// clicar botão
+// pegar o valor do input
+// fazer a validação e/ou criptografia - validort.js/cipher
+// retorno o valor para o usuario
+
+//seletores de DOM
+
+/*console.log(cipher);
+
+const button = document.getElementById("button")
+const title = document.getElementById("title")
+button.addEventListener("click", teste)
+
+const textearea = document.getElementById("input-texto")
+
+function teste() {
+    console.log(textearea.value)
+}*/
